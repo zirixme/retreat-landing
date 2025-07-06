1206e5f1e7c540e4dac71e4a39aca6f12ee5ba02
zirixme <zirix.me@gmail.com>
2025-07-06 13:28:43 +0300

    feat: used the button component instead of the default one

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,39 @@
+import Button from "./Button.jsx";
+
 function Navbar() {
   return (
-    <nav className="flex justify-between px-8 py-4 bg-transparent">
-      <div>
-        <ul>
-          <li>
-            <img src="/logo.svg" alt="" />
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul className="flex gap-10 items-center">
-          <li className="">
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Experiences</a>
-          </li>
-          <li>
-            <a href="#">Journal</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-          <li>
-            <button className="bg-button text-white px-4 py-2 rounded">
-              Get Started
-            </button>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <header className="w-full absolute top-0 left-0 z-10">
+      <nav className="hidden md:flex justify-between px-8 py-4 bg-transparent">
+        <div>
+          <ul>
+            <li>
+              <img src="/logo.svg" alt="" />
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul className="flex gap-10 items-center text-white">
+            <li className="">
+              <a href="#" className="">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">Experiences</a>
+            </li>
+            <li>
+              <a href="#">Journal</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <Button title={"Get Started"} />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 }
 export default Navbar;
