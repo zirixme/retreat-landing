64d58461cb38d24ddea70c8d0a451ec807886109
zirixme <zirix.me@gmail.com>
2025-07-06 16:56:30 +0300

    feat: add motion variants to use like defaults

---
+++ src/utils/motionVariants.js
@@ -0,0 +1,23 @@
+export const fadeInUp = {
+  hidden: {
+    opacity: 0,
+    y: 30,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const staggerContainer = {
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
