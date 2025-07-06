7f2df011cef728be3520d37657f89ce65c1c607b
zirixme <zirix.me@gmail.com>
2025-07-06 17:05:55 +0300

    style: add the top footer for styling

---
+++ src/components/TopFooter.jsx
@@ -0,0 +1,8 @@
+function TopFooter() {
+  return (
+    <div className="bg-green-950 h-12 flex items-center px-5">
+      <p className="text-gray-400">All right reserved ©</p>
+    </div>
+  );
+}
+export default TopFooter;
