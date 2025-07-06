1c4898f25069ec93bb3d1b9e6fd038d3ab2a5d2a
zirixme <zirix.me@gmail.com>
2025-07-06 16:59:07 +0300

    feat: add experiences component to be renderd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import Experiences from "./components/Experiences.jsx";
+import TopFooter from "./components/TopFooter.jsx";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <TopFooter />
+      <Experiences />
     </>
   );
 }
