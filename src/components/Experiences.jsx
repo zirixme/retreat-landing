ed905be664a488aec49f77d9a34cd28156548c6e
zirixme <zirix.me@gmail.com>
2025-07-06 17:56:31 +0300

    style: add animation for text and cards

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -1,43 +1,75 @@
 import ExperCard from "./ExperCard.jsx";
+import { motion } from "framer-motion";
+import {
+  fadeInUp,
+  staggerContainer,
+  imageFadeIn,
+} from "../utils/motionVariants.js";
+
 function Experiences() {
   return (
-    <section className="bg-background h-fit text-center pt-20 flex flex-col items-center">
-      <div className="max-w-screen-xl flex justify-center items-center gap-5 md:gap-10 mb-4">
-        <hr className="w-14 md:w-20" />
-        <h1 className="text-4xl md:text-5xl tracking-tight font-semibold">
+    <section className="bg-background h-fit text-center pt-20 flex flex-col items-center pb-20">
+      <motion.div
+        className="max-w-screen-xl flex justify-center items-center gap-10 mb-4"
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+      >
+        <hr className="w-0 md:w-20" />
+        <motion.h1
+          className="text-4xl md:text-5xl tracking-tight font-semibold"
+          variants={fadeInUp}
+        >
           Discover What Awaits You
-        </h1>
-        <hr className="w-14 md:w-20" />
-      </div>
-      <p className="mb-16 max-w-2xl ">
+        </motion.h1>
+        <hr className="w-0 md:w-20" />
+      </motion.div>
+      <motion.p
+        className="mb-16 max-w-2xl"
+        variants={fadeInUp}
+        initial="hidden"
+        whileInView="show"
+      >
         From mindful mornings to heart-pumping hikes, our experiences are
         crafted to reconnect you with the wild, with others, and with yourself.
         Whether you’re here to rest, explore, or grow we have something just for
         you.
-      </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
-        <ExperCard
-          img={"/foraging.jpg"}
-          title={"Foraging & Plant Walks"}
-          paragraph={
-            "Learn about local plants, herbs, and how nature nourishes us led by a certified herbalist."
-          }
-        />
-        <ExperCard
-          img={"/campfire.jpg"}
-          title={"Evening Fire Circles"}
-          paragraph={
-            "Gather around the fire to share stories, reflect, or simply enjoy the stars in good company."
-          }
-        />
-        <ExperCard
-          img={"/hike.jpg"}
-          title={"Guided Adventures"}
-          paragraph={
-            "Wander through alpine trails with expert guides who know every hidden path and story the forest holds."
-          }
-        />
-      </div>
+      </motion.p>
+      <motion.div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4"
+        whileInView="show"
+        initial="hidden"
+        variants={staggerContainer}
+        viewport={{ once: true, amount: 0.3 }}
+      >
+        <motion.div variants={imageFadeIn}>
+          <ExperCard
+            img={"/foraging.jpg"}
+            title={"Foraging & Plant Walks"}
+            paragraph={
+              "Learn about local plants, herbs, and how nature nourishes us led by a certified herbalist."
+            }
+          />
+        </motion.div>
+        <motion.div variants={imageFadeIn}>
+          <ExperCard
+            img={"/campfire.jpg"}
+            title={"Evening Fire Circles"}
+            paragraph={
+              "Gather around the fire to share stories, reflect, or simply enjoy the stars in good company."
+            }
+          />
+        </motion.div>
+        <motion.div variants={imageFadeIn}>
+          <ExperCard
+            img={"/hike.jpg"}
+            title={"Guided Adventures"}
+            paragraph={
+              "Wander through alpine trails with expert guides who know every hidden path and story the forest holds."
+            }
+          />
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
