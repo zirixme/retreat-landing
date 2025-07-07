73cc2cbee3aad1584d7d177b5c258a9b021b5063
zirixme <zirix.me@gmail.com>
2025-07-07 16:14:11 +0300

    feat: Copyright component

---
+++ src/components/Copyright.jsx
@@ -0,0 +1,15 @@
+function Copyright() {
+  return (
+    <div className="bg-black text-white/60 text-sm px-6 py-4 border-t border-white/10 text-center md:text-left">
+      © 2025 Wilderness Retreat. All rights reserved.{" "}
+      <a href="#" className="underline hover:text-white transition">
+        Privacy Policy
+      </a>{" "}
+      •{" "}
+      <a href="#" className="underline hover:text-white transition">
+        Terms of Service
+      </a>
+    </div>
+  );
+}
+export default Copyright;
